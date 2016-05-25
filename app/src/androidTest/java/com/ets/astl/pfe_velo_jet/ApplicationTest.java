dabadf7c96ea9c9da882ca43bc733e2ff9de76a0
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2016-05-25 10:59:24 -0400

    First project commit. (Android Studio scaffolding)

---
+++ app/src/androidTest/java/com/ets/astl/pfe_velo_jet/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ets.astl.pfe_velo_jet;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
